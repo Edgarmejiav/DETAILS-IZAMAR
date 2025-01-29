12a6b36b6ef80f21d3cc1235e4a66313e18bbb94
edgar <legamejiav@gmail.com>
2025-01-29 17:24:43 -0500

    ✨ style

--- app/components/Cart.tsx
+++ app/components/Cart.tsx
@@ -14,8 +14,8 @@ export default function Cart({cart, addToCart, removeFromCart}: CartProps) {
     const [expanded, setExpanded] = useState(false);
 
     return (
-        <div className="w-full sm:w-2/3 mb-4 sm:mb-0">
-            <h2 className=" flex justify-between text-xl font-bold text-purple-700 mb-2">Carrito de Compras
+        <div className="w-full mb-4">
+            <h2 className="  flex justify-between text-xl font-bold text-purple-700 mb-2">Carrito de Compras
                 {!expanded ? <svg onClick={() => setExpanded(!expanded)} xmlns="http://www.w3.org/2000/svg" fill="none"
                                   viewBox="0 0 24 24" stroke-width="1.5"
                                   stroke="currentColor" className="size-6">
@@ -31,9 +31,9 @@ export default function Cart({cart, addToCart, removeFromCart}: CartProps) {
             </h2>
             {
                 cart.length === 0 ? (
-                    <p className="text-gray-600">Tu carrito está vacío</p>
+                    <p className="text-gray-600 h-14 ">Tu carrito está vacío</p>
                 ) : (
-                    <ScrollArea className={`${expanded ? "md:h-1/3 h-1/2 " : "h-24"} w-full rounded-md border p-2`}>
+                    <ScrollArea className={`${expanded ? "md:h-1/3 h-1/2 " : "h-14"} w-full rounded-md border p-2`}>
                     <ul>
                             {cart.map((item) => (
                                 <li key={item.id} className="mb-2 last:mb-0 gap-3 flex justify-between items-center">
@@ -50,10 +50,10 @@ export default function Cart({cart, addToCart, removeFromCart}: CartProps) {
                                         </div>
                                         <div className="font-semibold text-purple-600">{item.name}</div>
                                         <div className="text-sm text-gray-600">
-                                            S/ {item.price.toFixed(2)} x
-                                            <strong> {item.quantity} </strong>
-                                            = S/ {(item.price * item.quantity).toFixed(2)}
+                                            {item.quantity !== 1 &&  <p> S/ {item.price.toFixed(2)} x <strong> {item.quantity} </strong>   = </p>}
+                                           S/ {(item.price * item.quantity).toFixed(2)}
                                         </div>
+
 
                                     </div>
 
--- app/page.tsx
+++ app/page.tsx
@@ -65,8 +65,8 @@ export default function Home() {
     }
 
     return (
-        <main className="min-h-screen bg-gradient-to-br from-pink-100 via-purple-100 to-blue-100 pb-32">
-            <div className="container mx-auto px-4 py-12">
+        <main className="min-h-screen container bg-gradient-to-br from-pink-100 via-purple-100 to-blue-100 pb-32">
+            <div className=" mx-auto px-4 py-12">
                 <header className="text-center">
                     <h1 className="text-2xl sm:text-4xl md:text-5xl font-bold mb-6 sm:mb-8 md:mb-12 text-purple-700 animate-pulse whitespace-nowrap overflow-hidden">
                         LITTLES DETAILS IZAMAR
@@ -74,9 +74,9 @@ export default function Home() {
                 </header>
                 <ProductList flowers={flowers} addToCart={addToCart} removeFromCart={removeFromCart} cart={cart}/>
             </div>
-            <div className="fixed bottom-0 left-0 right-0 bg-white shadow-lg">
+            <div className=" rounded-md fixed bottom-0 left-0 right-0 bg-white shadow-lg">
                 <div
-                    className="container mx-auto px-4 py-4 flex flex-col sm:flex-row justify-between items-start sm:items-center">
+                    className=" md:mx-52  md:gap-10  px-4 py-4 flex flex-col sm:flex-row justify-between items-start sm:items-center">
                     <Cart cart={cart} addToCart={addToCart} removeFromCart={removeFromCart}/>
                     <WhatsAppButton cart={cart} total={total}/>
                 </div>
