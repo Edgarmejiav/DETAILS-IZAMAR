3f45c400a28aa05ebdfbd5bebce3f9d58ae8bd10
edgar <legamejiav@gmail.com>
2025-01-29 17:00:23 -0500

    ✨ add carrito

--- app/components/Cart.tsx
+++ app/components/Cart.tsx
@@ -1,62 +1,83 @@
-import { ScrollArea } from "@/components/ui/scroll-area"
-import { Button } from "@/components/ui/button"
+import {ScrollArea} from "@/components/ui/scroll-area"
+import {Button} from "@/components/ui/button"
 import Image from "next/image";
 import {CartItem} from "@/app/page";
+import {useState} from "react";
 
 interface CartProps {
     cart: CartItem[]
-    addToCart: (flower:CartItem) => void
+    addToCart: (flower: CartItem) => void
     removeFromCart: (flowerId: number) => void
 }
-export default function Cart({ cart, addToCart, removeFromCart }:CartProps) {
-  return (
-    <div className="w-full sm:w-2/3 mb-4 sm:mb-0">
-      <h2 className="text-xl font-bold text-purple-700 mb-2">Carrito de Compras</h2>
-      {cart.length === 0 ? (
-        <p className="text-gray-600">Tu carrito está vacío</p>
-      ) : (
-        <ScrollArea className="h-24 w-full rounded-md border p-2">
-          <ul>
-            {cart.map((item) => (
-                <li key={item.id} className="mb-2 last:mb-0 gap-3 flex justify-between items-center">
-                    <div className="flex items-center space-x-4">
-                        <div className="font-semibold text-purple-600">{item.name}</div>
-                        <div className="text-sm text-gray-600">
-                            S/ {item.price.toFixed(2)} x {item.quantity} = S/ {(item.price * item.quantity).toFixed(2)}
-                        </div>
-                        <div className="relative w-16 h-16">
-                            <Image
-                                src={`/imgs/${item.name}.jpeg`}
-                                alt={item.name}
-                                layout="fill"
-                                objectFit="cover"
-                                objectPosition="top"
-                                className="rounded-md"
-                            />
-                        </div>
-                    </div>
 
-                    <div className="flex items-center">
-                        <Button
-                            onClick={() => removeFromCart(item.id)}
-                            className="bg-red-500 hover:bg-red-600 text-white font-bold py-1 px-2 rounded transition-colors duration-300 text-xs "
-                        >
-                            -
-                        </Button>
-                        <span className="text-sm font-bold mr-2">{item.quantity}</span>
-                        <Button
-                            onClick={() => addToCart(item)}
-                            className="bg-green-500 hover:bg-green-600 text-white font-bold py-1 px-2 rounded transition-colors duration-300 text-xs"
-                        >
-                            +
-                        </Button>
-                    </div>
-                </li>
-            ))}
-          </ul>
-        </ScrollArea>
-      )}
-    </div>
-  )
+export default function Cart({cart, addToCart, removeFromCart}: CartProps) {
+    const [expanded, setExpanded] = useState(false);
+
+    return (
+        <div className="w-full sm:w-2/3 mb-4 sm:mb-0">
+            <h2 className=" flex justify-between text-xl font-bold text-purple-700 mb-2">Carrito de Compras
+                {!expanded ? <svg onClick={() => setExpanded(!expanded)} xmlns="http://www.w3.org/2000/svg" fill="none"
+                                  viewBox="0 0 24 24" stroke-width="1.5"
+                                  stroke="currentColor" className="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 18.75 7.5-7.5 7.5 7.5"/>
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 7.5-7.5 7.5 7.5"/>
+                </svg> : <svg onClick={() => setExpanded(!expanded)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                              stroke="currentColor" className="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                          d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5"/>
+                </svg>}
+
+
+            </h2>
+            {
+                cart.length === 0 ? (
+                    <p className="text-gray-600">Tu carrito está vacío</p>
+                ) : (
+                    <ScrollArea className={`${expanded ? "md:h-1/3 h-1/2 " : "h-24"} w-full rounded-md border p-2`}>
+                    <ul>
+                            {cart.map((item) => (
+                                <li key={item.id} className="mb-2 last:mb-0 gap-3 flex justify-between items-center">
+                                    <div className="flex items-center space-x-4">
+                                        <div className="relative w-16 h-16">
+                                            <Image
+                                                src={`/imgs/${item.name}.jpeg`}
+                                                alt={item.name}
+                                                layout="fill"
+                                                objectFit="cover"
+                                                objectPosition="top"
+                                                className="rounded-md"
+                                            />
+                                        </div>
+                                        <div className="font-semibold text-purple-600">{item.name}</div>
+                                        <div className="text-sm text-gray-600">
+                                            S/ {item.price.toFixed(2)} x
+                                            <strong> {item.quantity} </strong>
+                                            = S/ {(item.price * item.quantity).toFixed(2)}
+                                        </div>
+
+                                    </div>
+
+                                    <div className="flex items-center">
+                                        <Button
+                                            onClick={() => removeFromCart(item.id)}
+                                            className="bg-red-500 hover:bg-red-600 text-white font-bold py-1 px-2 rounded transition-colors duration-300 text-xs "
+                                        >
+                                            -
+                                        </Button>
+                                        {/*<span className="text-sm font-bold mr-2">{item.quantity}</span>*/}
+                                        <Button
+                                            onClick={() => addToCart(item)}
+                                            className="bg-green-500 hover:bg-green-600 text-white font-bold py-1 px-2 rounded transition-colors duration-300 text-xs"
+                                        >
+                                            +
+                                        </Button>
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
+                    </ScrollArea>
+                )}
+        </div>
+    )
 }
 
--- app/components/ProductList.tsx
+++ app/components/ProductList.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button"
+import {Button} from "@/components/ui/button"
 import Image from "next/image"
 import {CartItem, Flower} from "@/app/page";
 
@@ -9,60 +9,69 @@ interface ProductListProps {
     cart: CartItem[]
 }
 
-export default function ProductList({ flowers, addToCart, removeFromCart, cart }: ProductListProps) {
-  const getItemQuantity = (flowerId:number) => {
-    const item = cart.find((item) => item.id === flowerId)
-    return item ? item.quantity : 0
-  }
+export default function ProductList({flowers, addToCart, removeFromCart, cart}: ProductListProps) {
+    const getItemQuantity = (flowerId: number) => {
+        const item = cart.find((item) => item.id === flowerId)
+        return item ? item.quantity : 0
+    }
 
-  return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-      {flowers.map((flower) => (
-          <div
-              key={flower.id}
-              className="border-2 border-pink-300 p-4 rounded-lg shadow-lg bg-gradient-to-br from-pink-100 to-purple-100 hover:shadow-xl transition-shadow duration-300"
-          >
-            <div className="relative h-64 mb-4">
-              <Image
-                  src={`/imgs/${flower.name}.jpeg`}
-                  alt={flower.name}
-                  layout="fill"
-                  objectFit="cover"
-                  objectPosition={"top"}
-                  className="rounded-md"
-              />
-            </div>
-            <h2 className="text-2xl font-bold text-black mb-2">{flower.name}</h2>
-            {/*<p className="text-gray-600 mb-3">{flower.description}</p>*/}
-            <div className="flex  justify-center items-center flex-row gap-3">
-              <p className="text font-bold text-gray-400 mb-4 line-through ">
-                S/ {(flowers[0].price + 5).toFixed(2)}
-              </p>
-              <p className="text-2xl font-bold text-[#E10714] mb-4">
-                S/ {flowers[0].price.toFixed(2)}
-              </p>
-            </div>
+    return (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+            {flowers.map((flower) => (
+                <div
+                    key={flower.id}
+                    className="border-2 border-pink-300 p-4 rounded-lg shadow-lg bg-gradient-to-br from-pink-100 to-purple-100
+                  transition-all duration-300"
+                >
+                    <div className="relative h-64 mb-4 overflow-hidden rounded-md">
+                        <Image
+                            src={`/imgs/${flower.name}.jpeg`}
+                            alt={flower.name}
+                            layout="fill"
+                            objectFit="cover"
+                            objectPosition="top"
+                            className="rounded-md transform transition-transform duration-300 hover:scale-125"
+                        />
+                    </div>
+                    <h2 className="text-2xl font-bold text-black mb-2">{flower.name}</h2>
 
+                    <div className="flex justify-center items-center flex-row gap-3">
+                        <p className="text font-bold text-gray-400 mb-4 line-through">
+                            S/ {(flower.price + 5).toFixed(2)}
+                        </p>
+                        <p className="text-2xl font-bold text-[#E10714] mb-4">
+                            S/ {flower.price.toFixed(2)}
+                        </p>
+                    </div>
+                    <Button
+                        onClick={() => addToCart(flower)}
+                        className="w-full bg-gradient-to-r from-green-400 to-blue-500
+             hover:from-green-500 hover:to-blue-600
+             text-white font-bold py-2 px-4 rounded-full text-sm sm:text-base
+             transition-all duration-300 flex items-center justify-center
+            gap-2"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={2}
+                            stroke="currentColor"
+                            className="w-5 h-5"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M2.25 3h2.736a.75.75 0 0 1 .705.497L6.75 6.75m0 0h13.085a.75.75 0 0 1 .726.913l-1.436 6.445a.75.75 0 0 1-.726.587H8.25m-1.5-8.25 1.5 8.25m-1.5-8.25H4.125m4.125 12a.75.75 0 1 1-1.5 0m10.5 0a.75.75 0 1 1-1.5 0"
+                            />
+                        </svg>
+                        Agregar al Carrito
+                    </Button>
 
-            <div className="flex justify-between items-center">
-              <Button
-                  onClick={() => removeFromCart(flower.id)}
-                  className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full transition-colors duration-300"
-                  disabled={getItemQuantity(flower.id) === 0}
-              >
-                -
-              </Button>
-              <span className="text-lg font-bold">{getItemQuantity(flower.id)}</span>
-              <Button
-                  onClick={() => addToCart(flower)}
-                  className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full transition-colors duration-300"
-              >
-                +
-              </Button>
-            </div>
-          </div>
-      ))}
-    </div>
-  )
+                </div>
+            ))}
+        </div>
+
+    )
 }
 
